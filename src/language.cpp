56752ce032be87f9e99662083e85dccd6a357673
Martin Mueller <MartinMueller2003@yahoo.com>
2022-06-21 06:41:06 -0400

    Language constants file

---
+++ src/language.cpp
@@ -0,0 +1,37 @@
+/*
+   File: language.h
+   Project: PixelRadio, an RBDS/RDS FM Transmitter (QN8027 Digital FM IC)
+   Version: 1.0
+   Creation: Dec-16-2021
+   Revised:  MAR-12-2022
+   Public Release:
+   Revision History: See PixelRadio.cpp
+   Project Leader: T. Black (thomastech)
+   Contributors: thomastech
+
+   (c) copyright T. Black 2021-2022.
+   Licensed under GNU GPL 3.0 and later, under this license absolutely no warranty is given.
+   This Code was formatted with the uncrustify extension.
+
+   Note 1: const String in flash uses zero ram and only a single instance in flash
+
+ */
+
+// ************************************************************************************************
+
+// ************************************************************************************************
+#include "Language.h"
+
+const PROGMEM char N_controllers             [] = "controllers";
+const PROGMEM char N_default                 [] = "default";
+const PROGMEM char N_DisplayFseqName         [] = "DisplayFseqName";
+const PROGMEM char N_durationSec             [] = "durationSec";
+const PROGMEM char N_enabled                 [] = "enabled";
+const PROGMEM char N_EnableDisplayFseqName   [] = "EnableDisplayFseqName";
+const PROGMEM char N_list                    [] = "list";
+const PROGMEM char N_MaxIdleSec              [] = "MaxIdleSex";
+const PROGMEM char N_message                 [] = "message";
+const PROGMEM char N_messages                [] = "messages";
+const PROGMEM char N_Messages                [] = "Messages";
+const PROGMEM char N_name                    [] = "name";
+const PROGMEM char N_sequences               [] = "sequences";
