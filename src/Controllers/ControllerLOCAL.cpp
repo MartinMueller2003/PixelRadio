b3750b9555d8f1146c1d61dd32db3fd38061c36b
Martin Mueller <MartinMueller2003@yahoo.com>
2022-07-19 16:38:16 -0400

    Updated default message set.

--- src/Controllers/ControllerLOCAL.cpp
+++ src/Controllers/ControllerLOCAL.cpp
@@ -90,9 +90,9 @@ void c_ControllerLOCAL::CreateDefaultMsgSet()
    // DEBUG_START;
 
    Messages.clear();
-   Messages.AddMessage(F("LOCAL"), F("Msg1"));
-   Messages.AddMessage(F("LOCAL"), F("Msg2"));
-   Messages.AddMessage(F("LOCAL"), F("Msg3"));
+   Messages.AddMessage(F("LOCAL"), F("Welcome to Our Drive-by Holiday Light Show"));
+   Messages.AddMessage(F("LOCAL"), F("For Safety Keep Automobile Running Lights On"));
+   Messages.AddMessage(F("LOCAL"), F("Please Drive Slowly and Watch Out for Children and Pets"));
 
    // DEBUG_END;
 }
