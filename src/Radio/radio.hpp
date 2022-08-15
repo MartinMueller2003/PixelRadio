2175ee37a8c92da1a9c9df8dcb0b11c5dc0a12ed
Martin Mueller <MartinMueller2003@yahoo.com>
2022-08-15 13:13:07 -0400

    Removed un-needed comment

--- src/Radio/radio.hpp
+++ src/Radio/radio.hpp
@@ -1,6 +1,6 @@
 #pragma once
 /*
-   File: radio.cpp
+   File: radio.hpp
    Project: PixelRadio, an RBDS/RDS FM Transmitter (QN8027 Digital FM IC)
    Version: 1.1.0
    Creation: Dec-16-2021
@@ -39,7 +39,6 @@ public:
     
 private:
     void        updateOnAirSign(void);
-    // String      LastMessageSent;
     
 };
 
