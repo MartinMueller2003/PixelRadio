c119fe0aa766c70f9b1163fc3a25c17a3ff12f58
Martin Mueller <MartinMueller2003@yahoo.com>
2022-08-18 14:33:28 -0400

    Made carrier status a stand alone class.

---
+++ src/Radio/Controls/RfCarrierStatus.hpp
@@ -0,0 +1,32 @@
+#pragma once
+/*
+   File: RfCarrierStatus.cpp
+   Project: PixelRadio, an RBDS/RDS FM Transmitter (QN8027 Digital FM IC)
+   Version: 1.1.0
+   Creation: Dec-16-2021
+   Revised:  Jun-13-2022
+   Revision History: See PixelRadio.cpp
+   Project Leader: T. Black (thomastech)
+   Contributors: thomastech
+
+   (c) copyright T. Black 2021-2022, Licensed under GNU GPL 3.0 and later, under this license absolutely no warranty is given.
+   This Code was formatted with the uncrustify extension.
+ */
+
+// *********************************************************************************************
+#include <Arduino.h>
+#include "StatusControl.hpp"
+
+// *********************************************************************************************
+class cRfCarrierStatus : public cStatusControl
+{
+public:
+
+    cRfCarrierStatus ();
+    virtual ~cRfCarrierStatus ()    {}
+};      // class cRfCarrierStatus
+
+extern cRfCarrierStatus RfCarrierStatus;
+
+// *********************************************************************************************
+// OEF
