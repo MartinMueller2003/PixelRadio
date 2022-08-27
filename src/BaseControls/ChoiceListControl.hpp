7bd66a1420ea11621c03ebfd0f2eb3814d443215
Martin Mueller <MartinMueller2003@yahoo.com>
2022-08-27 14:32:02 -0400

    Fixed a comment

--- src/BaseControls/ChoiceListControl.hpp
+++ src/BaseControls/ChoiceListControl.hpp
@@ -1,6 +1,6 @@
 #pragma once
 /*
-   File: ChoiceListControl.cpp
+   File: ChoiceListControl.hpp
    Project: PixelRadio, an RBDS/RDS FM Transmitter (QN8027 Digital FM IC)
    Version: 1.1.0
    Creation: Dec-16-2021
