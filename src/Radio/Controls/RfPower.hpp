ac92a5009b95b8c3e8a389187c641750ecab92a8
Martin Mueller <MartinMueller2003@yahoo.com>
2022-09-26 15:10:25 -0400

    Fixed a comment

--- src/Radio/Controls/RfPower.hpp
+++ src/Radio/Controls/RfPower.hpp
@@ -1,6 +1,6 @@
 #pragma once
 /*
-  *    File: RfPower.cpp
+  *    File: RfPower.hpp
   *    Project: PixelRadio, an RBDS/RDS FM Transmitter (QN8027 Digital FM IC)
   *    Version: 1.1.0
   *    Creation: Dec-16-2021
