053931d3b149555587cacfb42f458a967a272061
Martin <MartinMueller2003@yahoo.com>
2023-06-01 08:49:12 -0400

    Added reporting file system size

--- src/fileSys.cpp
+++ src/fileSys.cpp
@@ -331,7 +331,7 @@ const String makeWebGif (String fileName, uint16_t width, uint16_t height, Strin
 // ============================================================================================================================
 uint32_t GetFreeFsSpace()
 {
-    return PixelRadio_LittleFS.totalBytes() - PixelRadio_LittleFS.usedBytes();
+    return (PixelRadio_LittleFS.totalBytes() - PixelRadio_LittleFS.usedBytes());
 }
 
 // *********************************************************************************************
